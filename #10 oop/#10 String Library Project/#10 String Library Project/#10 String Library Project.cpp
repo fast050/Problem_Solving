3c6918690c80552a0d30b7e60a539f85003e04b8
Khalid Elfaki <56397578+fast050@users.noreply.github.com>
2025-04-09 04:21:51 -0700

    Add files via upload

---
+++ #10 oop/#10 String Library Project/#10 String Library Project/#10 String Library Project.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+#include <vector>
+#include "clsString.h"
+
+int main()
+{
+    clsString text;
+    cout << clsString::RemovePunctuationFromText("Welcome to Jordan, Jordan is a nice country; it's amazing.");
+    system("pause>0");
+}
